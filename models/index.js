920ce0a00fd6fa800a28b4a943cfc8663266a658
Randie <srandall1213@gmail.com>
2022-09-07 20:36:58 -0600

    updated index.js & category-routes.js

--- models/index.js
+++ models/index.js
@@ -4,13 +4,22 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category ✔️
+Product.belongsTo(Category, {
+  foreignKey: 'reader_id',
+});
 
-// Categories have many Products
+// Categories have many Products ✔️
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag) ✔️
+Product.belongsToMany(Tag, { through: ProductTag })
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag) ✔️
+Tag.belongsToMany(Product, { through: ProductTag })
 
 module.exports = {
   Product,
